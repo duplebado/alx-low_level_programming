040eff9653b0ba4e82716fcac3ee34149a1a98e5
duplebado <ajibadeadebayoengr@gmail.com>
2021-06-17 17:01:08 +0000

    print using puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 /**
- * main - Entry point
- * main - writes '"Programming is like building a multilingual puzzle' to the output stream
+ * main - Entry point .Writes '"Programming is like building a multilingual puzzle'
  * Return: Always 0 (Success)
  */
 int main(void)
