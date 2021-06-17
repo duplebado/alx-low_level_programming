00a4469b9d0f3270956e99af4fbbf5402e485834
duplebado <ajibadeadebayoengr@gmail.com>
2021-06-17 18:48:35 +0000

    print to standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-	char str[100] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19"
+	char str[100] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 	fprintf(stderr, "%s\n", str);
 	return (1);
 }
