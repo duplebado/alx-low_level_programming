1751f051c55c6f0e0ea868f44b7461f48c170def
duplebado <ajibadeadebayoengr@gmail.com>
2021-06-18 20:30:30 +0000

    update

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  * Return: Always 0 (Success)
@@ -14,6 +13,7 @@ int main(void)
 			putchar(num + '0');
 		else
 			putchar(num - 10);
+	}
 	putchar('\n');
 	return (0);
 }
