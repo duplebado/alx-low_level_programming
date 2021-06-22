2ffe6e580bad21402837831e2a0e9716033c9ccf
duplebado <ajibadeadebayoengr@gmail.com>
2021-06-22 05:08:34 +0000

    check for lowercase characters

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,1 +1,15 @@
+#include "holberton.h"
 
+/**
+ * islower - checks for lowercase character 
+ *
+ * Return: 1 (Success) OR 0 (Failure)
+ */
+ 
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
