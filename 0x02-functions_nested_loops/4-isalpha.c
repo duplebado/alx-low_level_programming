0b13cf666d37f79a753ccaa6a864d7b2be317706
duplebado <ajibadeadebayoengr@gmail.com>
2021-06-22 05:14:53 +0000

    checks for alphabetic characters

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * isalpha - checks for alphabetic character
+ *
+ * Return: 1 (Success) OR 0 (Failure)
+ */
+
+int _isalpha(int c)
+{
+        if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+                return (1);
+        else
+                return (0);
+}
+
