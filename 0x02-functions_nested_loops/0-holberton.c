dec914bb84292c50f7c89bce79481495b1efc1ee
duplebado <ajibadeadebayoengr@gmail.com>
2021-06-22 04:25:07 +0000

    print 'Holberton'

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ *  main - Print the word "Holberton"
+ *
+ *  Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 0;
+	char school_name[] = "Holberton";
+	
+	while (i < 9)
+	{	
+		_putchar(school_name[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
