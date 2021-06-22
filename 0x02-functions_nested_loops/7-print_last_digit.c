ee84a9b670dc63a97ae3d3e692772aa4e037097f
duplebado <ajibadeadebayoengr@gmail.com>
2021-06-22 20:43:13 +0000

    update

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -13,10 +13,11 @@ int print_last_digit(int n)
 	int last_digit;
 
 	if (n < 0)
-		n = (n * -1);
-	last_digit = n % 10;
+		last_digit = (n % 10) * -1;
+	else
+		last_digit = n % 10;
 
-	_putchar(last_digit + 48);
+	_putchar(last_digit + '0');
 
 	return (last_digit);
 }
