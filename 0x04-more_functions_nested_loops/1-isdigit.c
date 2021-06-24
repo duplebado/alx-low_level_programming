d3b442c0e30255cacca7d1844eafcdaf63761fae
duplebado <ajibadeadebayoengr@gmail.com>
2021-06-24 11:58:39 +0000

    check for a digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#includes "holberton.h"
+
+/*
+ * _isdigit - check if input is a digit
+ *
+ * @c: input to be checked
+ *
+ * Return: 1 OR 0
+ *
+ */
+
+int _isdigit(int c)
+{
+	return (c >= 0 && c <= 9);
+}
