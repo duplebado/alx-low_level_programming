285d7dcb68fb93f3b9ae334f10aa2f67f352fc10
duplebado <ajibadeadebayoengr@gmail.com>
2021-06-24 12:54:45 +0000

    multiply two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * mul - multiply two int parameters
+ *
+ * @a: int type number
+ * @b: int type number
+ *
+ * Return: product of a and b
+ *
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
