20af71b4778a26930f712a5a4ad5f538bff06077
duplebado <ajibadeadebayoengr@gmail.com>
2021-06-27 08:11:22 +0000

    print square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * print_square - prints a square on the terminal
+ *
+ * @size: Integer - length of the square to be printed
+ *
+ * Return: void
+ */
+
+void print_square(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\');
+		return;
+	}
+
+	int i;
+	int j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			_putchar('#');
+			_putchar('\n');
+		}
+	}
+}
