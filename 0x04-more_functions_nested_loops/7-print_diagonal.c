06fded15bc2cb21dd034066f1c9597d6e8ba2774
duplebado <ajibadeadebayoengr@gmail.com>
2021-06-27 07:44:41 +0000

    print diagonal line

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - prints a diagonal line on the terminal
+ *
+ * @n: Integer - number of times '\' should be printed
+ *
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+	int white_spaces;
+
+	for (i = 0; i < n; i++)
+	{
+		for (white_spaces = 0; white_spaces < i; white_spaces++)
+			_putchar(' ');
+
+		_putchar('\\');
+		_putchar('\n');
+	}
+
+	_putchar('\n');
+}
