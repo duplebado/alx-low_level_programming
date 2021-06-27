3856ee0b7d73fa438360c0fbcfec03a6b76016b2
duplebado <ajibadeadebayoengr@gmail.com>
2021-06-27 07:17:49 +0000

    print 0-14 ten times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - prints 0 - 14 ten times
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int times;
+	int num;
+
+	for (times = 0; times <= 10; times++)
+	{
+		for (num = 0; num <= 14; num++)
+		{
+			if (num > 9)
+				_putchar('1');
+
+			_putchar((num % 10) + '0');
+		}
+
+		_putchar('\n');
+	}
+}
