b9413df0fca966dc779fb5bc0abccf15d8fcc0df
duplebado <ajibadeadebayoengr@gmail.com>
2021-06-27 09:50:48 +0000

    print triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * print_triangle - prints a triangle
+ *
+ * @size: Integer - height of triange
+ *
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchat('\n');
+		return;
+	}
+
+	for (i = 1; i <= size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			if (size - j <= i)
+				_putchar('#');
+			else
+				_putchar(' ');
+		}
+
+		_putchar('\n');
+	}
+}
