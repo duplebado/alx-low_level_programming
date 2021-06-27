9722167036d43e9fbf662750fce2fe306c5b65e7
duplebado <ajibadeadebayoengr@gmail.com>
2021-06-27 09:26:16 +0000

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,11 +8,11 @@
 
 int main(void)
 {
-	int i, j;
+	int i;
 
 	for (i = 1; i < 100; i++)
 	{
-		if (i % 5 == 0 && i % 3 == 0)
+		if (i % 15 == 0)
 			printf("FizzBuzz ");
 		else if (i % 3 == 0)
 			printf("Fizz ");
