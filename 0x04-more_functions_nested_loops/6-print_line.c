1892bdac255572e0107fd57f835c32aab14c1e3d
duplebado <ajibadeadebayoengr@gmail.com>
2021-06-27 07:27:57 +0000

    draw straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * print_line - draws a straight line on terminal
+ *
+ * @n: Integer - number of times to print '_'
+ *
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		_putchar('_');
+
+}
