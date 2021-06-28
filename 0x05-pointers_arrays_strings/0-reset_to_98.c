7d45b4fa3fc7b4e0264270153f09b38c718658a8
duplebado <ajibadeadebayoengr@gmail.com>
2021-06-28 02:50:27 +0000

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - sets the integer to 98
+ * @*n: a pointer to the integer we are to set to 98
+ * Return: nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
