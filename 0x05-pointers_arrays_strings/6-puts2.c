2bdb8e2dae34681f2e6932f7d8f337a2326f4115
duplebado <ajibadeadebayoengr@gmail.com>
2021-06-30 01:35:49 +0000

    print every other character

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * puts2 - prints every other character of a string
+ *
+ * @str - Input string
+ *
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		_putchar(s[i]);
+		i += 2;
+	}
+
+	_putchar('\n');
+}
