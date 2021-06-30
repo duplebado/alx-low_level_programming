6502d96f381b860eefce54cace98790fdf695e75
duplebado <ajibadeadebayoengr@gmail.com>
2021-06-30 00:43:42 +0000

    reverve a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,22 +1,30 @@
 #include "holberton.h"
 
 /**
- * rev_string - prints a string in reverse
+ * rev_string - reverses a string
  *
- * @s: String to be printed
+ * @s: String to be reversed
  *
  * Return: void
  */
 
 void rev_string(char *s)
 {
-	if (*s != '\0')
-	{
-		rev_string(++s);
-		s--;
-	}
+	int i = 0;
+	
+	while (s[i])
+		i++;
+	int j = i / 2;
+	int right = 0;
 
-	_putchar(*s);
+	while (right != j)
+	{
+		char temp = s[right];
+		int left = i - right + 1;
+		s[right] = s[left];
+		s[left] = temp;	
+		right ++;
+	}
 }
 
 
