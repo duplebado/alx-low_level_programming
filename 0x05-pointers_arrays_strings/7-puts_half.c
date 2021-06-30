d46052ab4e8acc5a2dfcb14c1844916fa5797c4e
duplebado <ajibadeadebayoengr@gmail.com>
2021-06-30 03:36:44 +0000

    print last half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * puts_half - print half of a string
+ *
+ * @str: input string
+ *
+ * Result: void
+ */
+
+void puts_half(char *str)
+{
+	int i = 0;
+	int n;
+
+	while (*str != '\0')
+	{
+		i++;
+		s++;
+	}
+
+	if (i % 2 != 0)
+		n = (i - 1) / 2;
+	else
+		n = i / 2;
+
+	for (j = 0; j <= n; j++)
+		_putchar(str[n - j];
+
+	_putchar('\n');
+}	
+
