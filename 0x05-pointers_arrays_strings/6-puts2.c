7d41287e04da124bb64bd8d7a949d91b969c9ed3
duplebado <ajibadeadebayoengr@gmail.com>
2021-06-30 03:09:33 +0000

    print every other character

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -12,9 +12,10 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	while (*(str + i) != '\0' && *(str + (i -1)) != '\0')
+	while (*(str + i) != '\0')
 	{
-		_putchar(str[i]);
+		if( i != 0 && *(str + (i - 1)) != '\0')
+			_putchar(str[i]);
 		i += 2;
 	}
 
