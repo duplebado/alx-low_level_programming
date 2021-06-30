bc62af31ab476184e0575c8ea255e257dc297e00
duplebado <ajibadeadebayoengr@gmail.com>
2021-06-30 01:24:58 +0000

    reverse a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -21,7 +21,7 @@ void rev_string(char *s)
 	while (right != j)
 	{
 		char temp = s[right];
-		int left = i - right + 1;
+		int left = i - right - 1;
 		s[right] = s[left];
 		s[left] = temp;	
 		right ++;
