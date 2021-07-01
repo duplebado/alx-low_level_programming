68a70e1b9ba96030eea73f1c52fd4396e1536a5a
duplebado <ajibadeadebayoengr@gmail.com>
2021-07-01 10:02:13 +0000

    copy a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * _strncpy - copies a string up to @n bytes
+ *
+ * @dest: destination string; src string is copied to it
+ *
+ * @src: source string; contains content to be copied
+ *
+ * @n: number of bytes of src that should be copied to dest
+ *
+ * Return: pointer to dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (dest[i] != '\0' && i != n)
+	{
+		*(dest + i) = src[i];
+		i++;
+	}
+
+	return (dest);
+}
