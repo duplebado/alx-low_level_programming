88d5afdaba8e929b82d3feef6475e9cc89cedbbd
duplebado <ajibadeadebayoengr@gmail.com>
2021-07-01 07:59:57 +0000

    concatenate two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * _strcat - concatenates two strings
+ *
+ * @dest: destination string; src string is appended to it
+ *
+ * @src: source string; gets appended to dest string
+ * 
+ * Return: pointer to dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i = 0; j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+
+	while (src[j] != '\0')
+	{
+		*(dest + i) = src[j];
+		j++;
+		i++;
+	}
+
+	*(dest + i) = '\0';
+
+	return (dest); 
+}
