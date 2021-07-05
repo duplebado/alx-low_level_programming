8e8c69429dbb087f950d51cf4ab75eb10bfe5e6d
duplebado <59564099+duplebado@users.noreply.github.com>
2021-07-05 20:10:35 +0100

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,20 +1,25 @@
 #include "holberton.h"
 
 /**
- * *_memcpy - copies memory area
- * @dest: memory area
- * @src: source
- * @n: length of src
+ * *_memcpy - copies @n bytes from @src to @ dest
  *
- * Return: pointer to dest.
+ * @dest: destination memory area
+ *
+ * @src: source memory area
+ *
+ * @n: number of bytes
+ *
+ * Return: pointer to @dest
  */
+
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
 	{
-		dest[i] = src[i];
+		*(dest + i) = *(src + i);
 	}
+
 	return (dest);
 }
