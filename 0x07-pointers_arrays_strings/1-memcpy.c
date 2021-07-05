0b74614d65fff37f6bbc18c28143afae51e9538d
duplebado <59564099+duplebado@users.noreply.github.com>
2021-07-05 20:03:17 +0100

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -4,9 +4,9 @@
  * _memcpy - copies n bytes from memory area @src to
  * memory area @ dest
  *
- * @src: source memory area
- *
  * @dest: destination memory area
+ *
+ * @src: source memory area
  *
  * @n: number of bytes
  *
