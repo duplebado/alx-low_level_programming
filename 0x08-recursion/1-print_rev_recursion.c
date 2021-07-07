ca263c5634af9a73f932aea556aee26e4fa0e56d
duplebado <ajibadeadebayoengr@gmail.com>
2021-07-07 07:49:03 +0000

    _print_rev_recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,21 +1,18 @@
 #include "holberton.h"
 
 /**
- * _puts_recursion - prints a string followed by a new line
+ * _print_rev_recursion - prints a string in reverse
  *
- * @s - pointer to the string
+ * @s: pointer to the string
  *
  * Return: void
  */
 
-void _puts_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
-	if (s[0] == '\0')
-	{
-		_putchar('\n');
+	if (*s == '\0')
 		return;
-	}
 
-	_putchar(s[0]);
-	_puts_recursion(s + 1);
+	_print_rev_recursion(++s);
+	_putchar(*s);
 }
