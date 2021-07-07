62fd830242ff4120a90084036dc5765689c9e721
duplebado <ajibadeadebayoengr@gmail.com>
2021-07-07 09:42:43 +0000

    _strlen_recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "holberton"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ *
+ * @s - the string whose length is to be returned
+ *
+ * Return: an integer --> length of @s
+ */
+
+int _strlen_recursion(char *s)
+{
+	if(*s == '\0')
+		return 0;
+
+	return (1 + _strlen_recursion(s + 1));
+}
