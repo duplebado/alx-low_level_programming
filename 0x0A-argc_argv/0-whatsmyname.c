3dce3be5900604f1b82a74ca15845277e71db5f1
duplebado <ajibadeadebayoengr@gmail.com>
2021-07-12 14:24:59 +0000

    Add a program that prints its name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,9 @@
 #include "holberton.h"
 
+void print_string(char *str);
+
 /**
- * main - entry point, prints it's name
+ * main - entry point, prints its name
  *
  * @argc: integer, length of @argv
  *
@@ -10,16 +12,29 @@
  * Return: 0, success
  */
 
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	int i;
+	print_array_elements(argv[0]);
+	_putchar('\n');
 
-	while (arg[0][i] != '/0')
+	return (0);
+}
+
+/**
+ * print_string - prints all char of a string
+ *
+ * @str: pointer to string
+ *
+ * Return: void
+ */
+
+void print_string(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
 	{
-		_putchar(arg[0][i];
+		_putchar(str[i]);
 		i++;
 	}
-
-	_putchar('/n');
-	return (0);
 }
