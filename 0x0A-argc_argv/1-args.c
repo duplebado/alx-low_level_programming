e15e6e3f2bdb3f97f3a3f4d29bbe87fe000858a5
duplebado <ajibadeadebayoengr@gmail.com>
2021-07-12 08:25:10 +0000

    Add program that prints it's name

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,22 +3,16 @@
 /**
  * main - entry point, prints it's name
  *
- * @argc: integer, length of @argv
+ * @argc: @argv length
  *
  * @argv: one-dimensional array of strings, arguments of this program
  *
  * Return: 0, success
  */
 
-int main(int argc, char *argv[])
+int main(argc, char *argv[])
 {
-	int i;
-
-	while (arg[0][i] != '/0')
-	{
-		_putchar(arg[0][i];
-		i++;
-	}
+	print
 
 	_putchar('/n');
 	return (0);
