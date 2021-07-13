689c2f94590fbcae7355142bedf24f39b43ff117
duplebado <ajibadeadebayoengr@gmail.com>
2021-07-13 20:51:36 +0000

    An attempt

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,25 +12,25 @@
 
 int **alloc_grid(int width, int height)
 {
-	int i, j, *alloc_mem;
+	int i, j, **alloc_mem;
 
 	if (width <= 0 || height <= 0)
 		return ('\0');
 
-	alloc_mem = (int *)malloc(sizeof(int *) * height);
+	alloc_mem = malloc(sizeof(int *) * height);
 
 	if (alloc_mem == NULL)
 		return ('\0');
 
 	for (i = 0; i < height; i++)
 	{
-		*(alloc_mem + i) = malloc(sizeof(int) * width);
+		alloc_mem[i] = malloc(sizeof(int) * width);
 
 		if (alloc_mem[i] == NULL)
 			return ('\0');
 
 		for (j = 0; j < width; j++)
-			*(*(alloc_mem + i) + j) = 0;
+			alloc_mem[i][j] = 0;
 	}
 
 	return (alloc_mem);
