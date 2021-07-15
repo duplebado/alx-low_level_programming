cd8cd40a9479834ca2ff9090cc8053c90eb1cc87
duplebado <ajibadeadebayoengr@gmail.com>
2021-07-15 20:23:56 +0000

    Fix attempt

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,30 +16,32 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-char *strconc;
-unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
+	char *concat;
+	unsigned int len = n, index;
 
-while (s1 && s1[len1])
-len1++;
-while (s2 && s2[len2])
-len2++;
-if (n < len2)
-strconc = malloc(sizeof(char) * (len1 + n + 1));
-else
-strconc = malloc(sizeof(char) * (len1 + len2 + 1));
-if (!strconc)
-return (NULL);
-while (i < len1)
-{
-strconc[i] = s1[i];
-i++;
-}
-while (i < (len1 + n))
-{
-strconc[i] = s2[j];
-i++;
-j++;
-}
-strconc[i] = '\0';
-return (strconc);
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	for (index = 0; s1[index]; index++)
+		len++;
+
+	concat = malloc(sizeof(char) * (len + 1));
+
+	if (!concat)
+		return (NULL);
+
+	len = 0;
+
+	for (index = 0; s1[index]; index++)
+		concat[len++] = s1[index];
+
+	for (index = 0; s2[index] && index < n; index++)
+		concat[len++] = s2[index];
+
+	concat[len] = '\0';
+
+	return (concat);
 }
