a74197433a8ae38a2350aba0adcc3124e7e3b231
duplebado <ajibadeadebayoengr@gmail.com>
2021-07-15 18:41:52 +0000

    Fix error

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -20,7 +20,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *ptr = malloc(sizeof(s1) + n + 1);
 
 	if (ptr == NULL)
-		return '\0';
+		return ('\0');
 
 	for (i = 0; s1[i]; i++)
 		ptr[i] = s1[i];
