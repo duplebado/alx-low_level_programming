375b92f66a2d383ce1f9d24198a16453cac5ab3d
duplebado <ajibadeadebayoengr@gmail.com>
2021-07-17 05:41:23 +0000

    test

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,46 +1,52 @@
 #include <stdlib.h>
+/**
+ * _strlen - find length of a string
+ * @s: string
+ * Return: int
+ */
+
+unsigned int _strlen(char *s)
+{
+	unsigned int size = 0;
+
+	for (; s[size] != '\0'; size++)
+		;
+	return (size);
+}
 
 /**
- * string_nconcat - concatenates two strings
- *
- * @s1: a string to be concatenated
- *
- * @s2: a string to be concatenated
- *
- * @n: number of bytes of @s2 to be concatenated
- *
- * Return: pointer to newly allocated space in memory
- * with the concatenated string (SUCCESS) or
- * NULL if insufficient memory was available (FAILURE)
+ * *string_nconcat - concatenates two strings
+ * @s1: string 1
+ * @s2: string 2
+ * @n: first bytes of s2 to be used
+ * Return: pointer or NULL
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int i, j;
-	char *ptr;
+	char *m;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-		ptr = malloc(n + 1);
-	}
-	else
-	{
-		ptr = malloc(sizeof(*s1) + n + 1);
-	}
-
 	if (s2 == NULL)
 		s2 = "";
 
-	if (ptr == NULL)
-		return ('\0');
+	if (n < _strlen(s2))
+		m = malloc(_strlen(s1) + n * sizeof(char) + 1);
+	else
+		m = malloc(_strlen(s1) + _strlen(s2) + 1);
 
-	for (i = 0; s1[i]; i++)
-		ptr[i] = s1[i];
+	if (m == 0)
+		return (NULL);
 
-	for (j = 0; s2[j] && j < n; j++, i++)
-		ptr[i] = s2[j];
+	for (i = 0; s1[i] != '\0'; i++)
+		m[i] = s1[i];
 
-	ptr[i] = '\0';
-	return (ptr);
+	for (j = 0; s2[j] != '\0' && j < n; i++, j++)
+		m[i] = s2[j];
+
+	m[i] = '\0';
+
+	return (m);
 }
