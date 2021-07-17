01f59e294da099c3444d266b206f0de74f509b00
duplebado <ajibadeadebayoengr@gmail.com>
2021-07-17 11:59:06 +0000

    Fix error

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -25,7 +25,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (ptr == NULL)
 		return ('\0');
 
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < size i++)
 		*(ptr + i) = 0;
 
 	return ((void *)ptr);
