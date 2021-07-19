68244958cbc19bf40a87e9b2dd171fc65cb66049
duplebado <ajibadeadebayoengr@gmail.com>
2021-07-19 15:31:16 +0000

    Add init_dog function

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+ * init_dog - initiliaze a variable of type struct dog
+ * @d: pointer to struct dog
+ * @name: string literal
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d.name = name;
+	d.age = age;
+	d.owner = age;
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,15 @@
 #ifndef DOG_
 #define DOG_
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
 /**
  * struct dog - dog class
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
  */
+
 struct dog
 {
 	char *name;
