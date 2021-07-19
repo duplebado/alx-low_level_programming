313b204429710992de83ea16ee99bcb40d6d020e
duplebado <ajibadeadebayoengr@gmail.com>
2021-07-19 16:15:44 +0000

    Add print_dog function

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+
+/**
+ * print_dog - prints the data items of struct dog
+ * d: pointer to struct dog
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		printf("Name: %s", d->name ? d->name : "(nil)");
+
+		if (d->age)
+			printf("Age: %f", d->age);
+		else
+			printf("Age: %s", "(nil)");
+
+		printf("Owner: %s", d->owner ? d->owner : "(nil)");
+	}
+}
