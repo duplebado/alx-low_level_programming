6719e496a90c57a99223fe932f340cdb7ac6adf7
duplebado <ajibadeadebayoengr@gmail.com>
2021-07-21 13:41:07 +0000

    Complete task 1

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: pointer to a string
+ * @f: a callback function that prints @name
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,4 +1,6 @@
 #ifndef FUNCTION_POINTERS_
 #define FUNCTION_POINTERS_
 
+void print_name(char *name, void (*f)(char *));
+
 #endif /* #ifndef FUNCTION_POINTERS_ */
