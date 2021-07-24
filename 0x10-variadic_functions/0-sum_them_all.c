ef7c17d8f0adb1f3456434365b843b5ebe949539
duplebado <ajibadeadebayoengr@gmail.com>
2021-07-24 05:08:02 +0000

    Task 0 complete

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,23 +1,26 @@
 #include <stdarg.h>
 
 /**
- * sum_them_all - Computes the sum of its parameters
- * @n: The number of parameters
- * Return: The sum of the arguments
+ * sum_them_all - sum of all its parameters
+ * @n: total number of parameters
+ * Return: sum of value parameters
  */
+
 int sum_them_all(const unsigned int n, ...)
 {
+	int i, sum = 0;
+
+	if (n == 0)
+		return (sum);
+
 	va_list args;
-	unsigned int i;
-	int sum = 0;
 
-	if (n != 0)
-	{
-		va_start(args, n);
+	va_start(args, n);
 
-		for (i = 0; i < n; i++)
-			sum += va_arg(args, int);
-		va_end(args);
-	}
+	for (i = 0; i < n; i++)
+		sum += va_args(args, int);
+
+	va_end(args);
+
 	return (sum);
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,25 +1,6 @@
-#ifndef VARIADIC_FUNCTIONS_H
-#define VARIADIC_FUNCTIONS_H
-#include <stdarg.h>
-/**
- * struct format_printer - A structure for a type formatted printer
- * @type: The type of data to be formatted
- * @func: The format printing function
- */
-struct format_printer
-{
-	char type;
-	void (*func)(va_list *args_list);
-};
-typedef struct format_printer fmt_printer_t;
-int _putchar(char c);
-int sum_them_all(const unsigned int n, ...);
-void print_numbers(const char *separator, const unsigned int n, ...);
-void print_strings(const char *separator, const unsigned int n, ...);
-void print_char(va_list *args);
-void print_integer(va_list *args);
-void print_float(va_list *args);
-void print_string(va_list *args);
-void print_all(const char * const format, ...);
+#ifndef VARIADIC_FUNCTIONS_H_
+#define VARIADIC_FUNCTIONS_H_
 
-#endif /* #ifndef VARIADIC_FUNCTIONS_H */
+int _putchar(char c);
+
+#endif
