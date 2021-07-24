5017bf8a222252afd9880c162894f4a6c24c9b49
duplebado <ajibadeadebayoengr@gmail.com>
2021-07-24 05:14:11 +0000

    Fix gcc errors

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,17 +8,17 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-	int i, sum = 0;
+	unsigned int i;
+	int sum = 0;
+	va_list args;
 
 	if (n == 0)
 		return (sum);
 
-	va_list args;
-
 	va_start(args, n);
 
 	for (i = 0; i < n; i++)
-		sum += va_args(args, int);
+		sum += va_arg(args, int);
 
 	va_end(args);
 
