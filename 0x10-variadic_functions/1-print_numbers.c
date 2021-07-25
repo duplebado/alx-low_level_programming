a33b32a39b1c8b4401fd4f72f6b3373c01409852
duplebado <ajibadeadebayoengr@gmail.com>
2021-07-25 08:28:32 +0000

    Use appropriate separator

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -17,7 +17,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < (n - 1); i++)
 	{
 		if (*separator == '\0')
-			printf("%d ", va_arg(params, int));
+			printf("%d", va_arg(params, int));
 		else
 			printf("%d%s", va_arg(params, int), separator);
 	}
