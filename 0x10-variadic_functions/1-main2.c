3cba78039be9840d284aeb5fbaa8de79550cce04
duplebado <ajibadeadebayoengr@gmail.com>
2021-07-25 08:25:56 +0000

    Use appropriate separator

---
+++ 0x10-variadic_functions/1-main2.c
@@ -0,0 +1,12 @@
+#include "variadic_functions.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_numbers("", 4, 304, 76, 87236, 28376);
+	return (0);
+}
