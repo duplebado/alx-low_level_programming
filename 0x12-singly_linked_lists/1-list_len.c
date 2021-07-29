2c4fae7c190e732a2c74a4ec787a6eb17eb4764f
duplebado <ajibadeadebayoengr@gmail.com>
2021-07-29 11:51:26 +0000

    Change how to access string length

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -16,7 +16,7 @@ size_t print_list(const list_t *h)
 	while (h)
 	{
 		if (h->str)
-			printf("[%u] %s\n", h->len, h->str);
+			printf("[%d] %s\n", _strLen(h->str), h->str);
 		else
 			printf("[0] (nil)\n");
 
