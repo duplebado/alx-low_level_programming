5fa480614220e594554d5eb97bbf55d7c7a8fa6c
duplebado <ajibadeadebayoengr@gmail.com>
2021-08-02 11:07:36 +0000

    Complete task 4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint - frees a listint_t list
+ * @head: pointer to the head of the listint_t list
+ */
+
+void free_listint(listint_t *head)
+{
+	if (head->next)
+	{
+		free_listint(head->next);
+	}
+
+	free(head);
+}
