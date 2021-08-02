b332a83670b35fae70c631005c55cba666356d2b
duplebado <ajibadeadebayoengr@gmail.com>
2021-08-02 09:43:04 +0000

    Initialize project

---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,20 @@
+#ifndef _LISTS_H_
+#define _LISTS_H_
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+int _putchar(char c);
+
+#endif /* #ifndef _LISTS_H_ */
