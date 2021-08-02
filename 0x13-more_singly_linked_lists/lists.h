98c717edc6a190ac8d9f446c1664ad1d84462b91
duplebado <ajibadeadebayoengr@gmail.com>
2021-08-02 10:10:28 +0000

    Include stdlib and stdio in appropriate files

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,9 +1,6 @@
 #ifndef _LISTS_H_
 #define _LISTS_H_
 
-#include <stdlib.h>
-#include <stdio.h>
-
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -20,5 +17,6 @@ typedef struct listint_s
 
 int _putchar(char c);
 size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
 
 #endif /* #ifndef _LISTS_H_ */
