03d3072dee210d1307672aa90b00dffe137986f2
duplebado <ajibadeadebayoengr@gmail.com>
2021-08-03 08:49:24 +0000

    Fix null check

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,13 +10,10 @@ void free_listint(listint_t *head);
 
 void free_listint2(listint_t **head)
 {
-	if (!(*head))
+	if (!head)
 		return;
 
-	if ((*head)->next)
-		free_listint((*head)->next);
-
-	free(*head);
+	free_listint(*head)
 	*head = NULL;
 }
 
