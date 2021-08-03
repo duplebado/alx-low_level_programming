649666ae11b29d410446013c5d5ae8ededf8d4dc
duplebado <ajibadeadebayoengr@gmail.com>
2021-08-03 08:35:49 +0000

    Write helper function in same file

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include "lists.h"
 
+void free_listint(listint_t *head);
+
 /**
  * free_listint2 - frees a listint_t list and sets the head to NULL
  * @head: double pointer to the head of the listint_t list
@@ -14,3 +16,22 @@ void free_listint2(listint_t **head)
 	free_listint(*head);
 	*head = NULL;
 }
+
+/**
+ * free_listint - frees a listint_t list
+ * @head: pointer to the head of the listint_t list
+ */
+
+void free_listint(listint_t *head)
+{
+	if (!head)
+		return;
+
+	if (head->next)
+	{
+		free_listint(head->next);
+	}
+
+	free(head);
+}
+
