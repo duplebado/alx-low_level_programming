30acb6ed7e51e45a36da47363d42c306ad85e482
duplebado <59564099+duplebado@users.noreply.github.com>
2021-08-09 21:58:59 +0100

    Update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,28 +1,46 @@
-#include "main.h"
-
+#include "holberton.h"
 /**
- * append_text_to_file - Appends text at the end of a file
- * @filename: The name of the file to create
- * @text_content: The content to be added at the file
- *
- * Return: 1 if successful, otherwise -1
- */
+  * _strlen - length of a string
+  * @s: input char
+  * Return: length of a string
+**/
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		i++;
+	}
+	return (i);
+}
+/**
+* append_text_to_file - appends text at the end of a file.
+* @filename: file to append.
+* @text_content: info to append into the file.
+* Return: 1 on success, -1 on failure
+*/
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, i;
+	ssize_t nletters;
+	int file;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
-	fd = open(filename, O_RDWR | O_APPEND, 0600);
-	if (fd >= 0)
+	file = open(filename, O_WRONLY | O_APPEND);
+	if (file == -1)
 	{
-		for (i = 0; text_content != NULL && *(text_content + i) != '\0'; i++)
-		{
-			if (write(fd, text_content + i, 1) != 1)
-				return (-1);
-		}
-		close(fd);
-		return (1);
+		return (-1);
 	}
-	return (-1);
+	if (text_content)
+	{
+		nletters = write(file, text_content, _strlen(text_content));
+		if (nletters == -1)
+		{
+			close(file);
+			return (-1);
+		}
+	}
+	close(file);
+	return (1);
 }
