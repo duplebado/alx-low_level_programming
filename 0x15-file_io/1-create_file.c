9f10b4641a4a621f0e237dfc45f593f2b50df8d5
duplebado <59564099+duplebado@users.noreply.github.com>
2021-08-09 21:58:25 +0100

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,29 +1,46 @@
-#include "main.h"
-
+#include "holberton.h"
 /**
- * create_file - Creates a file with a given content
- * @filename: The name of the file to create
- * @text_content: The content of the file
- *
- * Return: 1 if successful, otherwise -1
- */
+  * _strlen - length of a string
+  * @s: input char
+  * Return: length of a string
+**/
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		i++;
+	}
+	return (i);
+}
+/**
+* create_file - check the code for Holberton School students.
+* @filename: file to create.
+* @text_content: info to write into the file.
+* Return: 1 on success, -1 on failure
+*/
 int create_file(const char *filename, char *text_content)
 {
-	int fd, i;
+	ssize_t nletters;
+	int file;
 
-	if (filename == NULL)
-		return (-1);
-	fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0600);
-	if (fd >= 0)
-	{
-		for (i = 0; text_content != NULL && *(text_content + i) != '\0'; i++)
-		{
-			if (write(fd, text_content + i, 1) != 1)
-				return (-1);
-		}
-		if (close(fd) == -1)
-			return (-1);
+	if (!filename)
 		return (1);
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (file == -1)
+	{
+		return (-1);
 	}
-	return (-1);
+	if (text_content)
+	{
+		nletters = write(file, text_content, _strlen(text_content));
+		if (nletters == -1)
+		{
+			close(file);
+			return (-1);
+		}
+	}
+	close(file);
+	return (1);
 }
