80cb53409ca33b251f256294e79d1d3190af0d06
duplebado <ajibadeadebayoengr@gmail.com>
2021-08-21 08:58:37 +0000

    Add 'out of range' check

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,5 +11,8 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int mask = 1;
 
+	if (index > (sizeof(unsigned long int) * 8 - 1))
+		return (-1);
+
 	return ((n >> index) & mask);
 }
