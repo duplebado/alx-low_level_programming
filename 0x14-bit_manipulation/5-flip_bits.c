fdd3c020874dada54a6646479ace7d0c614ebafd
duplebado <ajibadeadebayoengr@gmail.com>
2021-08-21 10:19:48 +0000

    Task 5

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,27 @@
+/**
+ * flip_bits - returns the number of bits you would need
+ * to flip to get from one number to another
+ * @n: a number
+ * @m: another number
+ * Return: the number of bits you would need
+ * to flip to get from @n to @m
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int result = 0, i = 0;
+	unsigned int limit = (sizeof(unsigned long int) * 8) - 1;
+
+	if (n == m)
+		return (result);
+	
+	while (i <= limit)
+	{
+		if (((n >> i) & 1) != ((m >> i) & 1))
+			result++;
+
+		i++;
+	}
+
+	return (result);	
+}
