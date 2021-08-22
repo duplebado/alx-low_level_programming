9505a9a7cf90a297b9dfa0ac485151f90b84c466
duplebado <ajibadeadebayoengr@gmail.com>
2021-08-22 18:07:56 +0000

    test

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,8 +1,4 @@
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <stdlib.h>
+#include "main.h"
 
 /**
  * read_textfile - Reads a text file and prints it to the POSIX standard output
@@ -11,33 +7,27 @@
  *
  * Return: The actual number of letters it could read and print, otherwise 0
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buf;
-	ssize_t fd, actual_read;
+	ssize_t n = 0;
+	int fd, a;
+	void *buf = malloc(sizeof(char) * 2);
 
+	if (filename == NULL || buf == NULL)
+		return (0);
 	fd = open(filename, O_RDONLY);
-
-	if (fd == -1)
+	if (fd < 0)
 		return (0);
-
-	buf = malloc(sizeof(char) * (letters + 1));
-
-	if (!buf)
-		return (0);
-
-	actual_read = read(fd, buf, letters);
-	buf[letters + 1] = '\0';
-
+	for (n = 0; n < (ssize_t)letters; n++)
+	{
+		a = read(fd, buf, 1);
+		if (a == 0)
+			break;
+		a = write(STDOUT_FILENO, buf, 1);
+		if (a != 1)
+			return (0);
+	}
 	close(fd);
-
-	actual_read = write(STDOUT_FILENO, buf, actual_read);
-
 	free(buf);
-
-	if (actual_read == -1)
-		return (0);
-
-	return (actual_read);
+	return (n);
 }
