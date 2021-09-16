aebf25e8aa63cd92324274a9b77c2f64abe1cdb2
duplebado <ajibadeadebayoengr@gmail.com>
2021-09-16 13:48:41 +0000

    Fix

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,5 +1,7 @@
 #include "lists.h"
 
+dlistint_t *_get_dnodeint_at_index(dlistint_t *head, unsigned int index);
+
 /**
  * insert_dnodeint_at_index - inserts a new node at a given position
  * @h: double pointer to the head of the list
@@ -14,23 +16,52 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *current_node_at_the_index, *new_node;
 
 	if (!(*h))
-		return (NULL)
+		return (NULL);
 
-	current_node_at_the_index = get_dnodeint_at_index(*h, idx);
+	current_node_at_the_index = _get_dnodeint_at_index(*h, idx);
 
 	if (!current_node_at_the_index)
 		return (NULL);
 
 	new_node = malloc(sizeof(dlistint_t));
-	
+
 	if (!new_node)
 		return (NULL);
 
 	new_node->n = n;
 	new_node->prev = current_node_at_the_index->prev;
-	new_node_>next = current_node_at_the_index;
+	new_node->next = current_node_at_the_index;
 	(current_node_at_the_index->prev)->next = new_node;
 	current_node_at_the_index->prev = new_node;
 
 	return (new_node);
+}
+
+/**
+ * _get_dnodeint_at_index - returns the nth node of a dlistint_t linked list
+ * @head: pointer to the head of the list
+ * @index: index of the node to returned
+ * Return: node at index @index, or
+ * if the node does not exist, return NULL
+ */
+
+
+dlistint_t *_get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *current_node = head;
+	unsigned int i = 0;
+
+	if (!head)
+		return (NULL);
+
+	while (current_node)
+	{
+		if (i == index)
+			return (current_node);
+
+		current_node = current_node->next;
+		i++;
+	}
+
+	return (NULL);
 }
