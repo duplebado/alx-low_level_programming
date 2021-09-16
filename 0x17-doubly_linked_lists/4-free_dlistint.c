39a5511b1a9d32401ab523dc609bfa3eb699b958
duplebado <ajibadeadebayoengr@gmail.com>
2021-09-16 12:42:46 +0000

    Complete task 4

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - frees a dlistint_t list
+ * @head: pointer to head of the dlistint_t list to be freed
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *current_node = head, *free_temp;
+
+	while (current_node->next)
+	{
+		free_temp = current_node;
+		current_node = current_node->next;
+		free(free_temp);
+	}
+
+	free(current_node);
+}
