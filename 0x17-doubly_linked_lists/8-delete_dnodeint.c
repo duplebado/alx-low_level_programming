34ba54ace6a2e37be4423350a47bff14ec739a20
duplebado <ajibadeadebayoengr@gmail.com>
2021-09-17 02:52:15 +0000

    Fixed betty error

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -4,7 +4,8 @@ int delete_first_node(dlistint_t *head);
 int delete_in_btw_nodes(dlistint_t *node);
 
 /**
- * delete_dnodeint_at_index - deletes the node at index index of a dlistint_t linked list
+ * delete_dnodeint_at_index - deletes the node at index index of a
+ * dlistint_t linked list
  * @head: double pointer to the head of the list
  * @index: index of the node to be deleted
  * Return: 1 if it succeeded, -1 if it failed
